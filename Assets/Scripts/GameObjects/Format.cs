35b77843736d066161568f11ee2ee06a960271c6
WonKangHee <rkdgml0820@korea.ac.kr>
2022-08-14 19:02:53 +0900

    Fix formats length

--- Assets/Scripts/GameObjects/Format.cs
+++ Assets/Scripts/GameObjects/Format.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Format : MonoBehaviour
 {
-    public GameObject scoreCheckObj;
+    [SerializeField]
+    private ScoreCheck scoreCheckObj;
+
     public int level;
+
     public int length;
-    public int weight;
 
     private void Start()
     {
-        GameObject scoreCheckLine = Instantiate(scoreCheckObj, transform.position + new Vector3(0, 8f * length, 0), transform.rotation);
+        GameObject scoreCheckLine = Instantiate(scoreCheckObj.gameObject, 
+            new Vector2(0, transform.position.y + length + 1), transform.rotation);
+        scoreCheckLine.transform.parent = transform;
         scoreCheckLine.GetComponent<ScoreCheck>().score = level;
     }
 
